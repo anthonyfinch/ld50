089e9fb30bf38927f5ab1e82122373311a5cbded
Anthony Finch <t@anthonyfinch.com>
2022-04-02 10:20:40 +0100

    initial commit - boilerplate setup

---
+++ Main.gd
@@ -0,0 +1,60 @@
+extends Control
+
+const TitleScene = preload("res://Title.tscn")
+
+export(Resource) var game_events
+
+onready var _scene_holder = $SceneHolder
+
+var _current_scene = null
+var _loader = null
+var _wait_frames = 0
+var _time_max = 100
+
+
+
+func _ready() -> void:
+	assert(game_events != null, "Please set game events resource")
+	_load_scene(TitleScene)
+
+	game_events.connect("change_scene", self, "_goto_scene")
+
+
+func _load_scene(scene: Resource) -> void:
+	_current_scene = scene.instance()
+	_scene_holder.add_child(_current_scene)
+
+
+func _goto_scene(name: String) -> void:
+	_loader = ResourceLoader.load_interactive(name + ".tscn")
+	assert(_loader != null)
+
+	set_process(true)
+
+	_current_scene.queue_free()
+
+	_wait_frames = 1
+
+
+func _process(_delta: float) -> void:
+	if _loader == null:
+		set_process(false)
+		return
+
+	if _wait_frames > 0:
+		_wait_frames -= 1
+		return
+
+	var t = OS.get_ticks_msec()
+
+	while OS.get_ticks_msec() < t + _time_max:
+		var err = _loader.poll()
+
+		if err == ERR_FILE_EOF:
+			var resource = _loader.get_resource()
+			_loader = null
+			_load_scene(resource)
+			break
+
+		elif err != OK:
+			assert(false, "Error loading scene: " + err)
