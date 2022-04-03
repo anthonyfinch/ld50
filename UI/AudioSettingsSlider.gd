e101ef992b76c307c3ee786a16b9e6baf1ee61cf
Anthony Finch <t@anthonyfinch.com>
2022-04-03 10:42:55 +0100

    adds first music track and fixes volume slider

--- UI/AudioSettingsSlider.gd
+++ UI/AudioSettingsSlider.gd
@@ -12,16 +12,15 @@ var _player = null
 func _ready():
 	assert(game_events != null, "Please set game events resource")
 
-	# setup to play audio hint on change
-	_player = AudioStreamPlayer.new()
-	_player.bus = bus_name
-	_player.stream = audio_hint
-	add_child(_player)
+	if audio_hint != null:
+		_player = AudioStreamPlayer.new()
+		_player.bus = bus_name
+		_player.stream = audio_hint
+		add_child(_player)
 
 	_bus_id = AudioServer.get_bus_index(bus_name)
 
-	# update from current setting
-	var current = pow(10, AudioServer.get_bus_volume_db(_bus_id)) / 20
+	var current = (1 - pow(10, AudioServer.get_bus_volume_db(_bus_id) / 20))
 	value = current * max_value
 
 	connect("value_changed", self, "_on_value_changed")
@@ -31,4 +30,5 @@ func _on_value_changed(value):
 	var ratio = value / max_value
 	var volume = max(-80.0, 20 * log(ratio))
 	AudioServer.set_bus_volume_db(_bus_id, volume)
-	_player.play()
+	if _player != null:
+		_player.play()
